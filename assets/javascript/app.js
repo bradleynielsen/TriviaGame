8c58415395a2ee940d32162941b6eaa3a6761a36
Brad <bradleynielsen@gmail.com>
2016-09-03 11:36:32 -0500

    are you afraid?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,72 @@
 $(document).ready( function() {
+// VARIABLES
+    var answers = [   
+        {   
+            question_text: "text",
+            answer_array: ["A0", "A1", "A2", "A3"],
+            answer_index: 2
+        },
+        {    
+            question_text: "text",
+            answer_array: ["A0", "A1", "A2", "A3"],
+            answer_index: 3
+        },
+
+        {
+            question_text: "text",
+            answer_array: ["A0","A1","A2","A3"],
+            answer_index: 1
+        }
+    ];
 
 
-var answers = [   
-    {   
-        question_text: "text",
-        answer_array: ["A0", "A1", "A2", "A3"],
-        answer_index: 2
-    },
-    {    
-        question_text: "text",
-        answer_array: ["A0", "A1", "A2", "A3"],
-        answer_index: 3
-    },
+    var r = 0;
+    var correct = false;
 
-    {
-        question_text: "text",
-        answer_array: ["A0","A1","A2","A3"],
-        answer_index: 1
-    }
-];
+// FUNCTIONS
+    // fn() ADD BUTTONS TO THE ANSWER DIV 
+    function createButtons(i) {
+        var newBtn = $("<button class='btn btn-outline-primary choiceBtn'>");
+            console.log(newBtn);
 
-
-var r = 0;
-var correct = false;
-
-
-
-function createButtons(i) {
-    var newBtn = $("<button class='btn btn-outline-primary'>");
-        console.log(newBtn);
-    var btnText = answers[r].answer_array[i];
-        console.log(btnText);
-    
-    //this is the index to store in the button to check the correct
-    var ansIndex = i; 
-
-    // adds the index of the answer to the button
-    newBtn.attr("data-answerIndex", ansIndex)
-    .text(btnText)
-    // .addclass(choiceBtn)
-    
-};
-
-
-
-// CLEAR THE BUTTONS
-    function clearButtons() {
-        $("#answers").empty();
-    }
-
-// loop through the answers to creat buttons
-function buttonLoop(){
-    for (var i = 0; i < answers[r].answer_array.length; i++) {
+        var btnText = answers[r].answer_array[i];
+            console.log(btnText);
         
-        createButtons(i);
-    }
-};
+        //this is the index to store in the button to check the correct
+        var ansIndex = i; 
+
+        // adds the index of the answer to the button
+        newBtn.attr("data-answerIndex", ansIndex)
+            .text(btnText)
 
 
-// WELCOME BUTTON
-$( "#welcomeBtn" ).click(function() {
-   // console.log(answers[r].answer_array.length)
-   buttonLoop();
-  
-});
+        $("#answers").append(newBtn);
+
+    };
+
+
+    // fn(): CLEAR THE ANSWERS DIV
+        function clearButtons() {
+            $("#answers").empty();
+        }
+
+    // fn(): ANSWER LOOP
+        function buttonLoop(){
+            for (var i = 0; i < answers[r].answer_array.length; i++) {
+                
+                createButtons(i);
+            }
+        };
+
+// ACTIONS AND CLICKS
+    // CLICK: WELCOME BUTTON
+    $( "#welcomeBtn" ).click(function() {
+       // console.log(answers[r].answer_array.length)
+       clearButtons();
+       buttonLoop();
+       $( "#welcomeBtn" ).hide();
+       $("#question").text()
+      
+    });
 
 });
