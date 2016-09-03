0cb371a9cd54f1ac9c53f74df856aacc1cf1d0c6
Brad <bradleynielsen@gmail.com>
2016-09-02 21:39:23 -0500

    buttons are not creating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,20 @@
+$(document).ready( function() {
+
 
 var answers = [   
-    {
-        question_text: "text"
+    {   
+        question_text: "text",
         answer_array: ["A0", "A1", "A2", "A3"],
         answer_index: 2
     },
-   {
-        question_text: "text"
+    {    
+        question_text: "text",
         answer_array: ["A0", "A1", "A2", "A3"],
         answer_index: 3
     },
 
-     {
-        question_text: "text"
+    {
+        question_text: "text",
         answer_array: ["A0","A1","A2","A3"],
         answer_index: 1
     }
@@ -23,36 +25,44 @@ var r = 0;
 var correct = false;
 
 
-//For each answer in answer_array, crate a button 
-     //n will be the index in the loop
-
-
-
-//
-
-for (var i = 0; i < answer_array.length; i++) {
-    createButtons(i);
-}
-
-
-// inserts the answer array data into the  button
 
 function createButtons(i) {
     var newBtn = $("<button class='btn btn-outline-primary'>");
+        console.log(newBtn);
     var btnText = answers[r].answer_array[i];
-    var ansIndex = i; /*this is the index to store in the button to check the correct*/
+        console.log(btnText);
+    
+    //this is the index to store in the button to check the correct
+    var ansIndex = i; 
 
     // adds the index of the answer to the button
     newBtn.attr("data-answerIndex", ansIndex)
-    .text(btnText);
-    .addclass(choiceBtn);
-}
+    .text(btnText)
+    // .addclass(choiceBtn)
+    
+};
 
 
-function clearButtons() {
-    $("#answers").empty();
-}
 
-$( "#welcomBtn" ).click(function() {
-  alert( "Handler for .click() called." );
+// CLEAR THE BUTTONS
+    function clearButtons() {
+        $("#answers").empty();
+    }
+
+// loop through the answers to creat buttons
+function buttonLoop(){
+    for (var i = 0; i < answers[r].answer_array.length; i++) {
+        
+        createButtons(i);
+    }
+};
+
+
+// WELCOME BUTTON
+$( "#welcomeBtn" ).click(function() {
+   // console.log(answers[r].answer_array.length)
+   buttonLoop();
+  
+});
+
 });
