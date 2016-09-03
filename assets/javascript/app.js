8d065f2faae53876b8305794c672259ca8702573
Brad Nielsen <Brad.Nielsen@hp.local>
2016-09-02 19:02:06 -0500

    added divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,57 @@
 
-    var answers = [   
-        {
-            question_text: "text"
-            answer_array: ["A0", "A1", "A2", "A3"],
-            answer_index: 2
-        },
-       {
-            question_text: "text"
-            answer_array: ["A0", "A1", "A2", "A3"],
-            answer_index: 3
-        },
+var answers = [   
+    {
+        question_text: "text"
+        answer_array: ["A0", "A1", "A2", "A3"],
+        answer_index: 2
+    },
+   {
+        question_text: "text"
+        answer_array: ["A0", "A1", "A2", "A3"],
+        answer_index: 3
+    },
 
-         {
-            question_text: "text"
-            answer_array: ["A0","A1","A2","A3"],
-            answer_index: 1
-        }
-    ];
+     {
+        question_text: "text"
+        answer_array: ["A0","A1","A2","A3"],
+        answer_index: 1
+    }
+];
+
 
 var r = 0;
+var correct = false;
+
 
 //For each answer in answer_array, crate a button 
      //n will be the index in the loop
 
 
 
-
+//
 
 for (var i = 0; i < answer_array.length; i++) {
-    createButton(i);
+    createButtons(i);
 }
 
 
 // inserts the answer array data into the  button
 
-function createButton(i) {
-    var newBtn = $("<button class='btn'>");
+function createButtons(i) {
+    var newBtn = $("<button class='btn btn-outline-primary'>");
     var btnText = answers[r].answer_array[i];
+    var ansIndex = i; /*this is the index to store in the button to check the correct*/
 
-    //next adds the index of the answer of where it is in the array
-    newBtn.attr("data-answerIndex", answers[r].answer_array[i]);
-    newBtn.text(btnText);
+    // adds the index of the answer to the button
+    newBtn.attr("data-answerIndex", ansIndex).text(btnText);
+    
+
+
+
+
+  
+
+
 }
 
 
