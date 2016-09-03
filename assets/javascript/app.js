3f98b0197404c98aeb58403526640b8b58497171
Brad <bradleynielsen@gmail.com>
2016-09-03 15:20:45 -0500

    adding checker soon?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,44 @@ $(document).ready( function() {
 // VARIABLES
     var answers = [   
         {   
-            question_text: "text",
-            answer_array: ["A0", "A1", "A2", "A3"],
+            question_text: "question text 1",
+            answer_array: ["anser 1", "A1", "A2", "A3"],
             answer_index: 2
         },
         {    
-            question_text: "text",
+            question_text: "question text 2",
             answer_array: ["A0", "A1", "A2", "A3"],
             answer_index: 3
         },
 
         {
-            question_text: "text",
+            question_text: "question text 3",
             answer_array: ["A0","A1","A2","A3"],
             answer_index: 1
         }
     ];
 
-
+//VARIABLES
     var r = 0;
     var correct = false;
+
+// ACTIONS AND CLICKS
+
+    // CLICK: WELCOME BUTTON
+    $( "#welcomeBtn" ).click(function() {
+       // console.log(answers[r].answer_array.length)
+       clearButtons();
+       buttonLoop();
+       $( "#welcomeBtn" ).hide();
+       $("#question").text(answers[r].question_text);
+       // $(".choiceBtn").on(click, checkAnswers);              
+    });
+
+    // give the answer buttons the click fn()
+    $( ".choiceBtn" ).click(function() {      
+       checkAnswer()
+      
+    });    
 
 // FUNCTIONS
     // fn() ADD BUTTONS TO THE ANSWER DIV 
@@ -38,10 +56,8 @@ $(document).ready( function() {
         // adds the index of the answer to the button
         newBtn.attr("data-answerIndex", ansIndex)
             .text(btnText)
-
-
         $("#answers").append(newBtn);
-
+        $("#answers").append("<span>");
     };
 
 
@@ -57,16 +73,23 @@ $(document).ready( function() {
                 createButtons(i);
             }
         };
+        function buttonClicked() {
+            delayButtonAlert = setTimeout( 3000);
+        }
 
-// ACTIONS AND CLICKS
-    // CLICK: WELCOME BUTTON
-    $( "#welcomeBtn" ).click(function() {
-       // console.log(answers[r].answer_array.length)
-       clearButtons();
-       buttonLoop();
-       $( "#welcomeBtn" ).hide();
-       $("#question").text()
-      
-    });
+        // // Clear the timeout.
+        // function cancelButtonAlert() {
+        //     clearTimeout(delayButtonAlert)
+        // }
+
+    // fn(): check answers
+        function checkAnswer(r) {
+            console.log(answers[r].answer_index);
+            var answrIndex = answers[r].answer_index;
+            console.log(answrIndex);
+
+        }
+
+
 
 });
